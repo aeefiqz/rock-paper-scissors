90b20929073d1cf14806f507d0bd25a5841dc731
Afiq Ezar <afiqezar96@gmail.com>
2022-12-15 06:40:21 +0800

    Created function for the button to listen on click event && show real time score of the game

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//create dom objects for respective buttons (rock,paper and scissors)
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+
+
+//create a div for displaying results
+const divResult = document.createElement('div');
+divResult.id = 'result';
+document.body.appendChild(divResult);
+
+//update the contents of results div
 function getComputerChoice() {
     // Randomly return "Rock","Paper" or "Scissors"
     //create an array for rock paper and scissors choices
@@ -9,81 +20,56 @@ function getComputerChoice() {
     return item
 }   
 
+function disableButton() {
+    buttons.forEach(elem => {
+        elem.disabled = true;
+    });
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection,computerSelection) {
     //Play a single round of Rock Paper Scissors and return a string that declares the winner 
     // of the round
     // playerSelection = playerSelection.toLowerCase();
+    let player = "";
+    let computer = "";
+    let result = "";
     if (playerSelection === computerSelection) {
-        return 'Tie';
-    }
-
-    else if (playerSelection == 'paper') {
-        if (computerSelection =='scissors') {
-            return "Computer Win!";
-        }else{
-            return "Player Win!";
-        }
-    }
-    else if (playerSelection == 'rock') {
-        if (computerSelection == 'paper') {
-            return "Computer win!";
-        }else{
-            return"Player win!";
-        }
-    }
-    else if (playerSelection == 'scissors') {
-        if (computerSelection == 'rock') {
-            return "Computer win!";
-        }else{
-            return "Player win!";
-        }
+        result = ` Draw! Player score: ${playerScore} vs Computer score: ${computerScore}`;
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection ==='scissors' && computerSelection === 'paper')
+    ) {
+        playerScore++;
+        player = `${playerScore}`;
+    } else {
+        computerScore++;
+        computer = `${computerScore}`;
+    
     }
     
-}
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+    document.querySelector(".player").innerHTML = `Player: ${playerScore}`;
+    document.querySelector(".computer").innerHTML = `Computer: ${computerScore}`;
+    document.querySelector(".result").innerHTML = `Result: ${result}`;
+ 
 
-        
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection,computerSelection);
-
-
-        if (result.includes("Win")) {
-            playerScore++;
-        } else if (result.includes("Lose")) {
-            computerScore++;
-        }
+    if (playerScore === 5) {
+        document.querySelector(".result").innerHTML = `Player wins!`
+        disableButton();
+    } else if (computerScore === 5) {
+        document.querySelector(".result").innerHTML = `Computer wins!`
+        disableButton();
     }
+}
+
+buttons.forEach(button => {
+    // for each one we add a 'click' listener
+    button.addEventListener("click", () => {
+        playRound(button.textContent, getComputerChoice());
+    });
+});
 
 
-
-//create dom objects for respective buttons (rock,paper and scissors)
-const rockbtn = document.querySelector('.btn-rock');
-const paperbtn = document.querySelector('.btn-paper');
-const scissorsbtn = document.querySelector('.btn-scissors');
-
-rockbtn.addEventListener('click', () => {
-    const computerSelection = getComputerChoice();
-    playRound('rock', computerSelection);
-  });
-  
-paperbtn.addEventListener('click', () => {
-    const computerSelection = getComputerChoice();
-    playRound('paper', computerSelection);
-  });
-  
-scissorsbtn.addEventListener('click', () => {
-    const computerSelection = getComputerChoice();
-    playRound('scissors', computerSelection);
-  });
-
-
-//create a div for displaying results
-const divResult = document.createElement('div');
-divResult.id = 'result';
-document.body.appendChild(divResult);
-
-//update the contents of results div
-divResult.innerHTML = `<p>Player score: ${playerScore}</p>
-                       <p>Computer score: ${computerScore}</p>`;
